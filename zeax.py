8cd72fb964b70fb0f512cd279ed621c27071b664
Zenith00 <zenith00dev@gmail.com>
2019-07-29 22:40:06 -0700

    owo hyperkronk

--- zeax.py
+++ zeax.py
@@ -1,36 +1,40 @@
-from aiohttp import web
+from aiohttp import web, ClientSession
 import ast
 from TextToOwO import owo
+
 routes = web.RouteTableDef()
+from TOKENS import smmry_key
+
+clientSession = ClientSession()
 
 
-@routes.get('/e')
-async def eval(request: web.Request):
-    print(request.query)
-    print(request.query_string)
+# @routes.get('/e')
+# async def eval(request: web.Request):
+#     print(request.query)
+#     print(request.query_string)
+#
+#     return web.Response(body="OK")
+#     pass
 
-    return web.Response(body="OK")
-    pass
+async def requ(query_url):
+    return (await clientSession.get(f"https://api.smmry.com&SM_API_KEY={smmry_key}&SM_URL={query_url}")).json()
 
 
 @routes.get('/jsonproxy')
 async def emb(request: web.Request):
+    req = await requ(request.query_string)
+
     return web.json_response({
         "type"   : "link",
         "version": 1.0,
-        "title"  : owo.text_to_owo(request.query_string)
+        "title"  : req["sm_api_content"]
     })
 
 
-@routes.get('/embed')
+@routes.get('/e')
 async def emb(request: web.Request):
     print(f"got query string {request.query_string}")
     return web.Response(text=f'<link type="application/json+oembed" href="http://ze.ax/jsonproxy?{request.query_string}" />', content_type="text/html")
-
-
-@routes.get('/embed2')
-async def emb(request: web.Request):
-    return web.Response(body="<head><title>The Rock (1996)</title></head>")
 
 
 app = web.Application()
